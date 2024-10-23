699fac14e7e6e3cd81c0844b2bfe7161a3945cab
Ludovico Mitchener <ludovico@phycoworks.com>
2024-10-23 22:37:32 +0100

    Simple working hosted deployment

--- k8s/manifests/kubernetes_manifest.tf
+++ k8s/manifests/kubernetes_manifest.tf
@@ -8,12 +8,15 @@ data "terraform_remote_state" "eks" {
 data "aws_eks_cluster" "cluster" {
   name = data.terraform_remote_state.eks.outputs.cluster_id
 }
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
 
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster.cluster.token
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 
@@ -24,8 +27,8 @@ locals {
   postgres_init_configmap_manifests = split("---\n", file("postgres-init-configmap.yaml"))
 
   # grafana_dashboard_manifest = file("grafana-dashboard.yaml")
-  # kube_state_metrics_manifests = split("---\n", file("kube-state-metrics.yaml"))
-  # prometheus_grafana_manifests = split("---\n", file("prometheus-grafana.yaml"))
+  kube_state_metrics_manifests = split("---\n", file("kube-state-metrics.yaml"))
+  prometheus_grafana_manifests = split("---\n", file("prometheus-grafana.yaml"))
 
 }
 
@@ -60,42 +63,15 @@ resource "kubernetes_manifest" "postgres_init_configmap_manifests" {
   depends_on = [kubernetes_namespace.simple_app]
 }
 
-# resource "kubernetes_manifest" "kube_state_metrics_manifests" {
-#   count = length(local.kube_state_metrics_manifests)
-#   manifest = yamldecode(local.kube_state_metrics_manifests[count.index])
-#   depends_on = [kubernetes_namespace.simple_app]
-#  }
-
-
-# resource "kubernetes_manifest" "grafana_dashboard_manifest" {
-#   manifest = {
-#     "apiVersion" = "v1"
-#     "kind" = "ConfigMap"
-#     "metadata" = {
-#       "name" = "grafana-dashboards"
-#       "namespace" = "simple-app"
-#     }
-#     "data" = {
-#       "dashboard.json" = replace(
-#         replace(local.grafana_dashboard_manifest, "!!json |", ""),
-#         "\n    ", "\n"
-#       )
-#     }
-#   }
-#   depends_on = [kubernetes_namespace.simple_app]
-# }
+resource "kubernetes_manifest" "kube_state_metrics_manifests" {
+  count = length(local.kube_state_metrics_manifests)
+  manifest = yamldecode(local.kube_state_metrics_manifests[count.index])
+  depends_on = [kubernetes_namespace.simple_app]
+ }
 
 
 
-# resource "kubernetes_manifest" "prometheus_grafana_manifests" {
-#   count = length(local.prometheus_grafana_manifests)
-#   manifest = yamldecode(local.prometheus_grafana_manifests[count.index])
-#   depends_on = [null_resource.wait_for_cluster, kubernetes_namespace.simple_app]
-
-#   // Add this block to handle the ServiceAccount resource
-#   lifecycle {
-#     ignore_changes = [
-#       manifest.metadata[0].namespace,
-#     ]
-#   }
-# }
+resource "kubernetes_manifest" "prometheus_grafana_manifests" {
+  count = length(local.prometheus_grafana_manifests)
+  manifest = yamldecode(local.prometheus_grafana_manifests[count.index])
+  depends_on = [kubernetes_namespace.simple_app]
